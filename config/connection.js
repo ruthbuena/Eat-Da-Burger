db497a251e0731f8c94d7ac1b327ed406f3d7d14
Ruth Buenaflor <roofio@Ruths-iMac.local>
2017-09-05 16:29:47 -0400

    addition of Heroku JawsDB URL credentials to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,21 @@
 // Used to connect Node to the DB created in MySQL
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+var connection;
 
-connection.connect(function(err){
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-    console.log("connected as id: " + connection.threadId);
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'dbuv3kusoxr4knnj',
+    password: 'bml4vlge9i2g32z8',
+    database: 'burgers_db'
+  });
+};
+
+
+connection.connect();
 
 // Export the connection
 module.exports = connection;
