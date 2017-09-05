c499df5dc15eb1de6b8f2f41dc2a6375cb03e412
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-09-05 15:56:22 -0400

    Add files via upload

---
+++ burgers_controller.js
@@ -0,0 +1,39 @@
+var express = require("express");
+
+// Create the router for the App
+var router = express.Router();
+
+// Import the model (burger.js) to use its DB methods
+var burger = require("../models/burger.js");
+
+// Initial Route
+router.get('/', function(req,res){
+  res.redirect('/index');
+});
+
+// Routes for the DB all,create and update methods
+router.get("/index", function(req,res){
+  burger.showAll(function(data){
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burger/create", function(req,res){
+  burger.insertBurger(req.body.burger_name, function(){
+    res.redirect("/index");
+  });
+});
+
+
+router.post("/burger/update/:id", function (req,res){
+  burger.updateBurger(req.params.id, function(){
+    res.redirect("/index");
+  });
+});
+
+// Export the Router
+module.exports = router;
